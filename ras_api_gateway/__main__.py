7332405e8793738937748569bbd1ab947da2b187
Gareth Bult <gareth@bult.co.uk>
2017-06-08 10:59:47 +0100

    Changing path

--- ras_api_gateway/__main__.py
+++ ras_api_gateway/__main__.py
@@ -19,14 +19,28 @@ from connexion import App
 from flask_cors import CORS
 from ras_api_gateway.proxy_router import router
 import logging
+from yaml import load
 
 if __name__ == '__main__':
     ons_env.activate()
     logging.getLogger('twisted').setLevel(logging.DEBUG)
     log.startLogging(stdout)
     client._HTTP11ClientFactory.noisy = True
-    app = App(__name__)
+    app = App(__name__, specification_dir='../swagger_server/swagger/')
     CORS(app.app)
+    try:
+        with open('../swagger_server/swagger/swagger.yaml') as io:
+            print("**** FOUND PATH 1")
+    except Exception:
+        print("#### 1 NOT FOUND")
+
+    try:
+        with open('swagger_server/swagger/swagger.yaml') as io:
+            specification = io.read()
+            print(load(specification))
+    except Exception:
+        print("#### 2 NOT FOUND")
+
     app.add_api('swagger.yaml', arguments={'title': 'ONS Microservice'})
     reactor.suggestThreadPoolSize(30)
     reactor.listenTCP(8080, ProxyFactory())

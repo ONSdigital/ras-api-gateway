37206a3389f29809efc03a56bc76e334ab65ca31
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2017-06-19 14:33:17 +0100

    Adding run.py scritp so we can bring up the ras-api-gateway in the same was as other ONS applications

--- run.py
+++ run.py
@@ -6,4 +6,31 @@
 #                                                                            #
 ##############################################################################
 
-#TO DO - create a run.py file.
+from twisted.internet import epollreactor
+epollreactor.install()
+from twisted.internet import reactor
+from twisted.python import log
+from twisted.web import client
+from flask_twisted import Twisted
+from sys import stdout
+from ras_api_gateway.factory_proxy import ProxyFactory
+from swagger_server.configuration import ons_env
+from connexion import App
+from flask_cors import CORS
+from ras_api_gateway.proxy_router import router
+import logging
+
+if __name__ == '__main__':
+    ons_env.activate()
+    logging.getLogger('twisted').setLevel(logging.DEBUG)
+    log.startLogging(stdout)
+    client._HTTP11ClientFactory.noisy = True
+    app = App(__name__, specification_dir='swagger_server/swagger/')
+    CORS(app.app)
+    app.add_api('swagger.yaml', arguments={'title': 'ONS Microservice'})
+    reactor.suggestThreadPoolSize(30)
+    reactor.listenTCP(8080, ProxyFactory())
+    reactor.callLater(1, router.setup)
+    Twisted(app).run(port=8079)
+
+

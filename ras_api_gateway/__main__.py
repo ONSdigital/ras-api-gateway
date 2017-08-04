db1f6ed65d53c310b42bda8255424e580144562e
Gareth Bult <gareth@bult.co.uk>
2017-08-04 16:20:34 +0100

    Adding more debug

--- ras_api_gateway/__main__.py
+++ ras_api_gateway/__main__.py
@@ -28,6 +28,7 @@ if __name__ == '__main__':
         port = int(getenv('PORT', 8080)) if not ons_env.cf.detected else ons_env.cf.port
         reactor.listenTCP(port, ProxyFactory())
         reactor.callLater(1, router.activate)
+        ons_env.logger.info('* api gateway listening on local port "{}"'.format(port))
 
     ons_env.activate(callback, twisted=True)
 
